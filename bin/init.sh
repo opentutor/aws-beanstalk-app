9a87dbf00a89a2b3e1235ecf4094922b0caaeca1
larry kirschner <larrykirschner@gmail.com>
2021-02-08 12:10:36 -0800

    fixes renamed upstream remote (#7)

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-UPSTREAM=https://github.com/opentutor/beanstalk-deployment.git
+UPSTREAM=https://github.com/opentutor/aws-beanstalk-app.git
 
+git remote remove upstream 2> /dev/null
 git remote add upstream $UPSTREAM
 git lfs install
 git fetch upstream
