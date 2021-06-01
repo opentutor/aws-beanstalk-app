d46b5d9f4027791ab7284a512e9ac0ae81a3a877
larry kirschner <larrykirschner@gmail.com>
2021-06-01 17:21:02 -0500

    2.3.x lr classifier w generated features (#14)
    * adds updateLessonFeatures gql mutation
    
    updates updateLesson gql mutation to take in a gql input object
    
    * updates README and version scripts
    
    * updated dialog manager. (#13)
    
    * installs LR arch w default classifier in deployed models
    
    * fixes: errors where untrained lesson (default classifier) would not work for some lessons
    
    * static service for all clients (admin, home, tutor)
    
    Co-authored-by: Kayla Carr <whizbon@gmail.com>
    Co-authored-by: Daniel Auerbach <dan100110@users.noreply.github.com>

--- bin/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-UPSTREAM=https://github.com/opentutor/aws-beanstalk-app.git
-
-git remote remove upstream 2> /dev/null
-git remote add upstream $UPSTREAM
-git lfs install
-git fetch upstream
-git reset --hard upstream/main
